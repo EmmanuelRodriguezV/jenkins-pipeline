5a0274120d840c1b8551ea124945a3874bc499c4
Emmanuel <erodriguez@fwscience.com>
2022-02-12 06:39:26 -0600

    another one

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          this should trigger react
+          this  is another build
         </a>
       </header>
     </div>
