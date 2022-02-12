db9ea9d6a609d2d0b4a9c24eb2d24c9d96ea794d
Emmanuel <erodriguez@fwscience.com>
2022-02-12 08:53:29 -0600

    add

--- src/App.js
+++ src/App.js
@@ -5,19 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         This code is edited
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          this  is another build
-        </a>
-        <h2>Mira este h2</h2>
+      <h4>mirame</h4>
       </header>
     </div>
   );
