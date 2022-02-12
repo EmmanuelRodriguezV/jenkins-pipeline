2854f99f2742a39d6ba962dc0377775976c6a4f4
Emmanuel <erodriguez@fwscience.com>
2022-02-12 06:38:34 -0600

    another try

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          this should trigger react
         </a>
       </header>
     </div>
